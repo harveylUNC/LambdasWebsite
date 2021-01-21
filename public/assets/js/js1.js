e0f4ca4c45811849ff061026441f2ba1d098d67a
unknown <harveyl@live.unc.edu>
2021-01-20 23:07:52 -0500

    loading static img test

--- public/assets/js/js1.js
+++ public/assets/js/js1.js
@@ -1,13 +1,22 @@
-function show(selClass){
+function show(selClass) {
     console.log(selClass);
     let classes = document.getElementsByClassName("classes");
     console.log(classes);
 
-    for(let i=0; i<classes.length;i++){
-        if(classes[i].id == selClass){
-            classes[i].style.display ="table";
+    for (let i = 0; i < classes.length; i++) {
+        if (classes[i].id == selClass) {
+            classes[i].style.display = "table";
         } else {
-            classes[i].style.display ="none";
+            classes[i].style.display = "none";
         }
     }
 }
+
+$('#cyberpunk').on('loadstart', function (event) {
+    $(this).addClass('loading');
+});
+
+$('#video_id').on('canplay', function (event) {
+    $(this).removeClass('loading');
+    $(this).attr('poster', '');
+});
